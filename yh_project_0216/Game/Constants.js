29fe2602fc57ae0bf31ed0be706925d38ba57ba6
yeaaaaahhhhh <72369871+yeaaaaahhhhh@users.noreply.github.com>
2022-04-04 13:36:54 -0500

    add shadowcheck on playerupdate

--- yh_project_0216/Game/Constants.js
+++ yh_project_0216/Game/Constants.js
@@ -2,5 +2,12 @@ class Constants{
     static wallW=10
     static startX=100
     static startY=100
+    static endy = 500
+    static endl = 450
+    static endr = 500
+    static walls=[]
+    static darkRect = []
     
+    static endOfGame = false;
 }
+export default Constants
--- yh_project_0216/Game/PlayerUpdateComponent.js
+++ yh_project_0216/Game/PlayerUpdateComponent.js
@@ -1,5 +1,6 @@
 import Component from "../engine/Component.js";
 import Input from '../engine/Input.js'
+import Constants from './Constants.js'
 class PlayerUpdateComponent extends Component {
     constructor(parent) {
         super(parent)
@@ -18,21 +19,53 @@ class PlayerUpdateComponent extends Component {
             switch (Input.frameKey) {
                 case "a":
                     pxNew = px - 5
-                    circle.x = pxNew
+                    
                     break;
                 case "s":
                     console.log("down")
                     pyNew = py + 5
-                    circle.y = pyNew
+                    
                     break;
                 case "d":
                     pxNew = px + 5
-                    circle.x = pxNew
+                    
                     break
                 case "w":
                     pyNew = py - 5
-                    circle.y = pyNew
+                    break;    
+            }
+            let collisionCheck = false;
+            for (let rect of Constants.walls) {
+                if (rect.x < pxNew + 10 && rect.x + rect.w > pxNew - 10 && rect.y < pyNew + 10 && rect.y + rect.h > pyNew - 10) {
+                    console.log("in")
+                    collisionCheck = true;
                     break;
+                }
+            }
+            if (collisionCheck == false) {
+                circle.x=pxNew
+                circle.y=pyNew
+            }
+
+
+            let idxfound = Constants.darkRect.findIndex(function (element) {
+                
+                if (element.x < circle.x + 10 && element.x + 50 > circle.x - 10 && element.y < circle.y + 10 && element.y + 50 > circle.y - 10) {
+                    //console.log("a")
+                    return true
+                }
+                return false
+            })
+
+            //console.log(idxfound)
+
+            if (idxfound > -1) {
+                //console.log(idxfound)
+                Constants.darkRect.splice(idxfound, 1)
+            }
+
+            if (Constants.endy < circle.y && (circle.x > Constants.endl && circle.x < Constants.endr)) {
+                Constants.endOfGame = true
             }
         }
     }
