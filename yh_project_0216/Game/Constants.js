8702b4c8259f45f014db69c376971803d88eb80a
yeaaaaahhhhh <72369871+yeaaaaahhhhh@users.noreply.github.com>
2022-04-26 23:48:56 -0500

    Update Constants.js

--- yh_project_0216/Game/Constants.js
+++ yh_project_0216/Game/Constants.js
@@ -5,9 +5,5 @@ class Constants{
     static endy = 500
     static endl = 450
     static endr = 500
-    static walls=[]
-    static darkRect = []
-    
-    static endOfGame = false;
 }
 export default Constants
